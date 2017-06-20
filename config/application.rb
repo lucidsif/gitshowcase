80c1b30a6cfd63c64340eccc5dc705cb6ae50fef
Tawsif <ahmedt93@gmail.com>
2017-06-20 08:25:50 +0000

    first commit

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Gitshowcase
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.web_console.whiny_requests = false
     config.exceptions_app = self.routes
     config.active_job.queue_adapter = :sidekiq
   end
